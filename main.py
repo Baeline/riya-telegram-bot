f60da988297489fa6241c2585212f75747ecee8a
Baeline <itsfashionbee@gmail.com>
2025-06-29 01:36:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/au
 creds_dict = json.loads(GOOGLE_CREDS_JSON)
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
-sheet = client.open("Riya Logs").sheet1
+sheet = client.open("Riya conversations").sheet1
 
 # Log to Sheet
 def log_to_sheet(user_id, lang_code, user_msg, reply, message_count=1):
